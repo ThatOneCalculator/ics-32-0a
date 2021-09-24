b800983f35588cc1708cb923f1908781744151ff
ThatOneCalculator <kainoa@t1c.dev>
2021-09-23 18:18:33 -0700

    avid diva

--- 8.rs
+++ 8.rs
@@ -1,6 +1,6 @@
 fn main() {
-	let palindrome = "RACECAR";
+	let palindrome = "avid diva";
 	let reverse_palindrome = palindrome.chars().rev().collect::<String>();
 	let result = if palindrome == reverse_palindrome{ "is" } else { "is not" };
-	println!("{} {} a palindrome.", palindrome, result); // should be "RACECAR is a palindrome."
+	println!("{} {} a palindrome.", palindrome, result); // should be "avid diva is a palindrome."
 }
