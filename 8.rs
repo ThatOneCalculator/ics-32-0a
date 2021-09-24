4590f217ecd0b490bbd6b9b639d505842a6d1215
ThatOneCalculator <kainoa@t1c.dev>
2021-09-23 18:16:41 -0700

    Finish 8

--- 8.rs
+++ 8.rs
@@ -1,3 +1,6 @@
 fn main() {
-
+	let palindrome = "RACECAR";
+	let reverse_palindrome = palindrome.chars().rev().collect::<String>();
+	let result = if palindrome == reverse_palindrome{ "is" } else { "is not" };
+	println!("{} {} a palindrome.", palindrome, result); // should be "RACECAR is a palindrome."
 }
