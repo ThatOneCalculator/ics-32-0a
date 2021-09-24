180cb2058a31100a4d400b7b85fc08078ff4b15d
ThatOneCalculator <kainoa@t1c.dev>
2021-09-23 18:09:35 -0700

    Finish 7

--- 7.rs
+++ 7.rs
@@ -1,7 +1,12 @@
 fn main() {
-	let string_to_search = String::new();
-	let char_vec: Vec<char> = s.chars().collect();
-	for c in char_vec {
-		println!("{}", c);
+	let string_to_search = "I'd just like to interject for a moment. What you're refering to as Linux, is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux.";
+	let char_to_search: char = 'r';
+	let char_vec: Vec<char> = string_to_search.chars().collect();
+	let mut count = 0;
+	for i in char_vec {
+		if i == char_to_search {
+			count += 1;
+		}
 	}
+	println!("{}", count); // should be 7
 }
